2a1aa253b1b33960a8baed1dd91de8bc859d27a2
Bernard Rodrigues <bernard.rodrigues@outlook.com>
2021-07-09 11:01:24 -0300

    NLW6 - Dia3 - Discover - Inclusao de cards, swiper e scrollReveal

--- main.js
+++ main.js
@@ -29,3 +29,29 @@ window.addEventListener('scroll', function () {
         header.classList.remove('scroll')
     }
 })
+
+// Testimonials Slider
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+})
+
+// Scroll Reveal - Mostra novos elementos durante a rolagem da página
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true,
+})
+
+scrollReveal.reveal(`
+    #home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links
+    `, { interval: 100 })
